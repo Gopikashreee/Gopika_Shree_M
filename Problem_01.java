d07308330c3e7b516b9f4e980eb2b9eaeb556d48
Gopika shree M <132184222+Gopikashreee@users.noreply.github.com>
2025-12-03 16:44:37 +0530

    solution updated

--- Problem_01.java
+++ Problem_01.java
@@ -1,1 +1,27 @@
 
+import java.util.Scanner;
+
+public class Problem_01 {
+   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter your inputs a and b : ");
+    double a = sc.nextDouble();
+    double b = sc.nextDouble();
+    sc.nextLine();
+    System.out.println("Choose operator: +, -,*, / :  ");
+    String operator = sc.nextLine();
+    switch(operator){
+        case "+": System.out.print(a+b);
+        break;
+        case "-": System.out.print(a-b);
+        break;
+        case"*": System.out.print(a*b);
+        break;
+        case"/": System.out.print(a/b);
+        break;
+        default: System.out.print("Invalid operator");
+
+    }
+
+   } 
+}
