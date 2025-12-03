f50e0c160ba1101b57397bc2511ae94861e7ffd3
Gopika shree M <132184222+Gopikashreee@users.noreply.github.com>
2025-12-03 16:46:46 +0530

    solution updated

--- Problem_04.java
+++ Problem_04.java
@@ -1,1 +1,28 @@
 
+import java.util.*;
+
+public class Problem_04 {
+    public static Map<Integer, Integer> getMultiplesCount(int[] arr) {
+        Map<Integer, Integer> result = new HashMap<>();
+
+        for (int i = 1; i <= 9; i++) {
+            result.put(i, 0);
+        }
+
+        for (int num : arr) {
+            for (int i = 1; i <= 9; i++) {
+                if (num % i == 0) {
+                    result.put(i, result.get(i) + 1);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] input = {1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30};
+        Map<Integer, Integer> output = getMultiplesCount(input);
+        System.out.println(output);
+    }
+}
