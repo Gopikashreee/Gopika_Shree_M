e35b5e081c663dd01563c38009014270e062073c
Gopika shree M <132184222+Gopikashreee@users.noreply.github.com>
2025-12-03 16:45:10 +0530

    solution updated

--- Problem_03.java
+++ Problem_03.java
@@ -1,1 +1,17 @@
 
+import java.util.Scanner;
+
+public class Problem_02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a >= 1){
+            System.out.print(1+" ");
+        }
+        if(a >= 2){
+            for(int i = 1;i<a;i++){
+                System.out.print((2*i)+1+" ");
+            }
+        }
+    }
+}
